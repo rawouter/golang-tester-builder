892bf7eeda1fc66508cfdf6ae1fb0097764e5919
Raphael Wouters <rawouter@cisco.com>
2016-12-08 11:12:21 +0100

    Removing GCFLAG

--- test_and_build.sh
+++ test_and_build.sh
@@ -7,7 +7,7 @@ source /build_environment.sh
 
 # Compile statically linked version of package
 echo "Building $pkgName"
-`CGO_ENABLED=${CGO_ENABLED:-0} go build -a ${FLAGS:-} --ldflags="${LDFLAGS:-}" --gcflags "${GCFLAGS:--N -l}" $pkgName`
+`CGO_ENABLED=${CGO_ENABLED:-0} go build -a ${FLAGS:-} --ldflags="${LDFLAGS:-}" $pkgName`
 
 
 # Run tests and compute coverage
